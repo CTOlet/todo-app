c45890c599f555ef46540fe4a53c1900a29aa56b
patrickap <patrick.schlageter@web.de>
2022-03-24 15:47:34 +0100

    build: added initial todo data

--- server/database/init.sql
+++ server/database/init.sql
@@ -1,12 +1,12 @@
 CREATE TABLE public.todos (
-  id int PRIMARY KEY,
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   title varchar(255),
   details varchar(255)
 );
 
 INSERT INTO public.todos 
-  (id, title, details)
+  (title, details)
 VALUES
-  (1, 'Title 1', 'Details 1'),
-  (2, 'Title 2', 'Details 2'),
-  (3, 'Title 3', 'Details 3');
+  ('Title 1', 'Details 1'),
+  ('Title 2', 'Details 2'),
+  ('Title 3', 'Details 3');
