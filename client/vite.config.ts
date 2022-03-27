3c725edca026ac664940532189854434f1973a45
patrickap <patrick.schlageter@web.de>
2022-03-27 20:15:52 +0200

    chore: initial setup of client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,15 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
+  publicDir: '/src/assets',
   server: {
     host: true,
     port: 3000,
-    // hmr: {
-    //   port: 3000,
-    // },
-    // watch: {
-    //   usePolling: true,
-    // },
   },
   plugins: [react()],
 });
