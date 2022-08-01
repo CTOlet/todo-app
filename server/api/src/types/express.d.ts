0d0356d7ab02a3eeb6e3a239352211f948f8e0cf
patrickap <patrick.schlageter@web.de>
2022-08-01 20:00:50 +0200

    fix: ts-node not recognizing d.ts. files

--- server/api/src/types/express.d.ts
+++ server/api/src/types/express.d.ts
@@ -1,16 +1,18 @@
 import { User } from '@prisma/client';
 import { Request, Response } from 'express';
 
-declare module 'express-serve-static-core' {
-  interface Request {
-    user?: User;
-  }
-  interface Response {
-    error: (options?: { code?: number; message?: string }) => void;
-    success: <T>(options?: {
-      code?: number;
-      message?: string;
-      data?: T;
-    }) => void;
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+    interface Response {
+      error: (options?: { code?: number; message?: string }) => void;
+      success: <T>(options?: {
+        code?: number;
+        message?: string;
+        data?: T;
+      }) => void;
+    }
   }
 }
